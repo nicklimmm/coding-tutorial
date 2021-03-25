33d969472f90adb97f63d113bc55bbf62ec5fa4a
nicklimmm <nickylim.p@gmail.com>
2021-03-25 12:39:31 +0800

    added day2 outline

---
+++ day2.py
@@ -0,0 +1,8 @@
+"""
+Outline:
+1. Update operators (+=, -=, ...)
+2. Conditional Statements (if, else, elif)
+3. Logical Operators (==, !=, and, or, ...)
+4. for and while loops
+5. Control flow statements (break, continue, pass)
+"""
