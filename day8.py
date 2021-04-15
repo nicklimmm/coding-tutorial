ba91e9f29b74386452b11b1047066199718ab30e
Nicky Lim <nickylim.p@gmail.com>
2021-04-15 16:50:39 +0800

    added day8 outline

---
+++ day8.py
@@ -0,0 +1,11 @@
+"""
+Outline:
+1. Scopes and Namespaces
+2. Functional and Object-Oriented Programming Paradigm
+3. Classes, Objects, Methods
+"""
+
+"""
+Namespace - works like 'dictionary' (keys -> object names, value -> values of the object).
+Scopes - the region of a program in which that name has meaning.
+"""
